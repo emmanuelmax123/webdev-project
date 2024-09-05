9eba51a4650981cb29052c29e0362e3be776835a
emmanuelmax123 <sanyaolukolawole202@gmail.com>
2024-09-05 20:57:50 +0200

    save cart to local storage

--- learing Js/13 Amazon Project/javascript-amazon-project-main/data/cart.js
+++ learing Js/13 Amazon Project/javascript-amazon-project-main/data/cart.js
@@ -1,13 +1,23 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+// this changes the "cart" from string back to an array (this is due to it beign saved in local storage)
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  // converts cart into a sting so it can be saved in local storage
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addTocart(productId) {
   let matchingItem;
@@ -24,6 +34,7 @@ export function addTocart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -34,4 +45,5 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
 }
