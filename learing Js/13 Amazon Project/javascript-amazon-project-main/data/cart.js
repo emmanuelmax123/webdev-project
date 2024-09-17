130e5eba1a193a092f4acff2b23075eb4f11af4a
emmanuelmax123 <sanyaolukolawole202@gmail.com>
2024-09-17 13:29:51 +0200

    save data and generate html for delivery options

--- learing Js/13 Amazon Project/javascript-amazon-project-main/data/cart.js
+++ learing Js/13 Amazon Project/javascript-amazon-project-main/data/cart.js
@@ -6,10 +6,12 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionId: "1",
     },
     {
       productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
       quantity: 1,
+      deliveryOptionId: "2",
     },
   ];
 }
@@ -32,6 +34,7 @@ export function addTocart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
