006bea96523bc8aa7c2d5487da31ddf9e9610b60
emmanuelmax123 <sanyaolukolawole202@gmail.com>
2024-09-21 19:05:54 +0200

    remove defaut test and add money test

---
+++ learing Js/13 Amazon Project/javascript-amazon-project-main/test-jasmine/moneyTest.js
@@ -0,0 +1,21 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+describe("test suite: format currency", () => {
+  it("converts cents into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+});
+
+describe("for edge case", () => {
+  it("works with 0", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("rounds up to the nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+
+  it("rounds down", () => {
+    expect(formatCurrency(2000.4)).toEqual("20.00");
+  });
+});
