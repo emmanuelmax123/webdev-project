7cbf2e517ff91b097a72ed49923e3d7d30e3c410
emmanuelmax123 <sanyaolukolawole202@gmail.com>
2024-10-02 12:30:02 +0200

    add backend practice file

--- learing Js/13 Amazon Project/javascript-amazon-project-main/scripts/checkout.js
+++ learing Js/13 Amazon Project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { rendercheckoutHeader } from "./checkout/checkoutHeader.js";
 import renderOrderSummary from "./checkout/ordersummary.js";
 import renderPaymentSummary from "./checkout/paymentsummary.js";
+import "../data/backend-practice.js";
 // import "../data/cart-class.js";
 import "../data/car.js";
 rendercheckoutHeader();
