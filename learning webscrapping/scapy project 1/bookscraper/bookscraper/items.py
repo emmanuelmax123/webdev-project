ea1a1dcf5666815300bd3ad6bc7cf5ab7d8a1fcc
emmanuelmax123 <sanyaolukolawole202@gmail.com>
2024-11-15 12:09:41 +0100

    cleaning data with pipelines

--- learning webscrapping/scapy project 1/bookscraper/bookscraper/items.py
+++ learning webscrapping/scapy project 1/bookscraper/bookscraper/items.py
@@ -10,3 +10,19 @@ class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    product_type = scrapy.Field()
+    price = scrapy.Field()
+    price_excel_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    reviews = scrapy.Field()
+    stars = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    
+
--- learning webscrapping/scapy project 1/bookscraper/bookscraper/pipelines.py
+++ learning webscrapping/scapy project 1/bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,44 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+
+
+        ##remove all whitespace 
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != "description":
+                Value = adapter.get(field_name)
+                adapter[field_name] = Value[0].strip()
+
+        ##availability ---> only show the numbers of books
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array)<2:
+            adapter["availability"] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter["availability"] = int(availability_array[0])
+
+        #Review --> convert string review into an interger
+        review_string = adapter.get('reviews')
+        adapter["reviews"] = int(review_string)
+
+        #Stars --> Convert star rating into int
+        star_rating = adapter.get("stars")
+        star_rating_array = star_rating.split(" ")
+        star_text_value = star_rating_array[1].lower()
+        if star_text_value == "zero":
+            adapter["stars"] = 0
+        elif star_text_value == 'one':
+            adapter["stars"] = 1
+        elif star_text_value == 'two':
+            adapter["stars"] = 2
+        elif star_text_value == 'three':
+            adapter["stars"] = 3
+        elif star_text_value == 'four':
+            adapter["stars"] = 4
+        elif star_text_value == 'five':
+            adapter["stars"] = 5
         return item
