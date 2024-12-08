cd71dc484468a1fa54b2a9c3acf084a4f4c3fb28
emmanuelmax123 <sanyaolukolawole202@gmail.com>
2024-12-08 07:08:42 +0100

    Revert "failed"
    This reverts commit 5ac4afbc5111f74d1eacf96acc49e7bb72cfe51c.

--- ReactNative/react-native-demo/src/componenets/Viewcomponet.tsx
+++ ReactNative/react-native-demo/src/componenets/Viewcomponet.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
-import "../globas.css";
 
 const Viewcomponet = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View>
       <Text>A view componet is exactly the same as a div in html </Text>
     </View>
   );
